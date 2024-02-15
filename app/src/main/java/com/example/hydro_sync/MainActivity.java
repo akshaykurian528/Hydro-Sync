be21ea2d8de61c3b4c6f0b0972065e5741f8f96a
Akshay <akshaykurian528@gmail.com>
2024-02-16 00:32:27 +0530

    fourth commit

--- app/src/main/java/com/example/hydro_sync/MainActivity.java
+++ app/src/main/java/com/example/hydro_sync/MainActivity.java
@@ -5,7 +5,17 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.hydro_sync.databinding.ActivityMainBinding;
 
@@ -18,27 +28,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        replaceFragement(new home());
+        replaceFragment(new home());
         binding.bottomNavigationView.setSelectedItemId(R.id.menuhome);
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.menuprofile) {
-                replaceFragement(new profile());
+                replaceFragment(new profile());
             } else if (item.getItemId() == R.id.menuhome) {
-                replaceFragement(new home());
+                replaceFragment(new home());
             } else if (item.getItemId() == R.id.menusettings) {
-                replaceFragement(new settings());
+                showBottomSheet();
             }
             return true;
         });
-
     }
 
-    public void replaceFragement(Fragment fragment){
+    private void showBottomSheet() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottomsheetlayout);
 
+        LinearLayout logout = dialog.findViewById(R.id.layoutlogout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Logout is pressed", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable((Color.TRANSPARENT)));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+    }
+
+    public void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
         fragmentTransaction.commit();
     }
 }
